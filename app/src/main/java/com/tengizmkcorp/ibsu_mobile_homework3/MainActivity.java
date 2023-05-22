7b3e56ee235606b613c9dcef049c449a5f533d31
tengiz <tengiz152@gmail.com>
2023-05-22 07:12:35 +0400

    bottom navigation switches between activities

--- app/src/main/java/com/tengizmkcorp/ibsu_mobile_homework3/MainActivity.java
+++ app/src/main/java/com/tengizmkcorp/ibsu_mobile_homework3/MainActivity.java
@@ -1,16 +1,23 @@
 package com.tengizmkcorp.ibsu_mobile_homework3;
 
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.tengizmkcorp.ibsu_mobile_homework3.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
     private ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,40 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-    }
+        setup();
+}
 
+    private void setup() {
+
+// Obtain NavController from FragmentContainerView
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        assert navHostFragment != null;
+        navController = navHostFragment.getNavController();
+// Set up BottomNavigationView
+        BottomNavigationView bottomNavigation = binding.bottomNavigationView;
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.mainFragment:
+                        navController.navigate(R.id.mainFragment);
+                        return true;
+                    case R.id.discoverFragment:
+                        navController.navigate(R.id.discoverFragment);
+                        return true;
+                    case R.id.reelsFragment:
+                        navController.navigate(R.id.reelsFragment);
+                        return true;
+                    case R.id.addFragment:
+                        navController.navigate(R.id.addFragment);
+                        return true;
+                    case R.id.sendFragment:
+                        navController.navigate(R.id.sendFragment);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
 }
--- app/src/main/java/com/tengizmkcorp/ibsu_mobile_homework3/ReelsFragment.java
+++ app/src/main/java/com/tengizmkcorp/ibsu_mobile_homework3/ReelsFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PostsFragment#newInstance} factory method to
+ * Use the {@link ReelsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PostsFragment extends Fragment {
+public class ReelsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class PostsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public PostsFragment() {
+    public ReelsFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class PostsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PostsFragment.
+     * @return A new instance of fragment ReelsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static PostsFragment newInstance(String param1, String param2) {
-        PostsFragment fragment = new PostsFragment();
+    public static ReelsFragment newInstance(String param1, String param2) {
+        ReelsFragment fragment = new ReelsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,8 +58,7 @@ public class PostsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_posts, container, false);
+        return inflater.inflate(R.layout.fragment_reels, container, false);
     }
 }
